16360b873b779559e1321ec68678e69213e938da
Anthony Raborn <araborn@pasco.k12.fl.us>
2020-10-27 14:07:43 -0400

    Added switch for bestECFA to choose model based on if objective function was maximized or not. Reworked bestECFA and modelTableToString to use exploratorySA S4 object.

--- R/mecfa_final_functions.R
+++ R/mecfa_final_functions.R
@@ -1,6 +1,7 @@
 #' Pick the best-fit ECFA model(s)
 #'
 #' @param object A fit MECFA list.
+#' @param max Was the objective function maximized (`TRUE`)?
 #'
 #' @export
 #'
@@ -8,48 +9,50 @@ bestECFA <-
   function(object, max = TRUE) {
     all_final_fit <-
       c()
-    for (i in 1:(length(object)-1)) {
+    results <-
+      object@modelResults
+    for (i in 1:(length(results)-1)) {
       all_final_fit[i] <-
-        object[[i]]@best_fit
+        results[[i]]@best_fit
     }
     if (max) {
-      object[[which(all_final_fit==max(all_final_fit))]]
+      results[[which(all_final_fit==max(all_final_fit))]]
     } else {
-      object[[which(all_final_fit==min(all_final_fit))]]
+      results[[which(all_final_fit==min(all_final_fit))]]
     }
   }
 
 #' Print the model in string format
 #'
-#' @param object A fit MECFA list.
+#' @param model A fit MECFA list.
 #'
 #' @export
 #'
 modelTableToString <-
-  function(object) {
+  function(model) {
     # convert to model.syntax for printing
-    factors = unique(object@best_syntax$lhs[which(object@best_syntax$op=='=~')])
+    factors = unique(model@best_syntax$lhs[which(model@best_syntax$op=='=~')])
     model.syntax = c()
     for (i in factors) {
-      factorRows = which(object@best_syntax$lhs==i&
-                           object@best_syntax$op=="=~"&
-                           object@best_syntax$free>0)
+      factorRows = which(model@best_syntax$lhs==i&
+                           model@best_syntax$op=="=~"&
+                           model@best_syntax$free>0)
       model.syntax = c(model.syntax,
                        paste0(i, " =~ ",
-                              paste0(object@best_syntax$rhs[factorRows],
+                              paste0(model@best_syntax$rhs[factorRows],
                                      collapse = " + "
                               )
                        )
       )
     }
     for (i in factors) {
-      factorRows = which(object@best_syntax$lhs==i&
-                           object@best_syntax$op=="~~"&
-                           object@best_syntax$free>0)
+      factorRows = which(model@best_syntax$lhs==i&
+                           model@best_syntax$op=="~~"&
+                           model@best_syntax$free>0)
       if (length(factorRows) > 0) {
         model.syntax = c(model.syntax,
                          paste0(i, " ~~ ",
-                                paste0(object@best_syntax$rhs[factorRows],
+                                paste0(model@best_syntax$rhs[factorRows],
                                        collapse = " + "
                                 )
                          )

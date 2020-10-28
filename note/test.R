0e5fa7effe96b1375983133ef6cc7be2d0ba1909
Anthony Raborn <araborn@pasco.k12.fl.us>
2020-10-28 14:28:15 -0400

    Continued updating of test script to match recent changes

--- note/test.R
+++ note/test.R
@@ -30,6 +30,12 @@ f2 =~ Item5 + Item2 + Item13
 f3 =~ Item11 + Item16 + Item8 + Item9
 f4 =~ Item15 + Item12 + Item4 + Item6 + Item20 + Item18"
 # Outcome ~ f1 + f2 + f3 + f4"
+startModel4c <-"
+f1 =~ Item1 + Item2 + Item3 + Item4 + Item5 + Item6 + Item7 + Item8 + Item9 + Item10 + Item11 + Item12 + Item13 + Item14 + Item15 + 0*Item16 + Item17 + Item18 + Item19 + Item20
+f2 =~ 0*Item1 + Item2 + Item3 + Item4 + Item5 + Item6 + Item7 + Item8 + Item9 + Item10 + Item11 + Item12 + Item13 + Item14 + Item15 + Item16 + Item17 + Item18 + Item19 + Item20
+f3 =~ Item1 + Item2 + Item3 + Item4 + Item5 + 0*Item6 + Item7 + Item8 + Item9 + Item10 + Item11 + Item12 + Item13 + Item14 + Item15 + Item16 + Item17 + Item18 + Item19 + Item20
+f4 =~ Item1 + Item2 + Item3 + Item4 + Item5 + Item6 + Item7 + Item8 + Item9 + Item10 + 0*Item11 + Item12 + Item13 + Item14 + Item15 + Item16 + Item17 + Item18 + Item19 + Item20
+"
 startModel2 <-"
 f1 =~ Item1 + Item2 + Item3 + Item4 + Item5 + Item6 + Item7 + Item8 + Item9 + Item10
 f2 =~ Item11 + Item12 + Item13 + Item14 + Item15 + Item16 + Item16 + Item17 + Item19 + Item20"
@@ -62,12 +68,12 @@ f5 =~ Item16 + Item17 + Item18 + Item19 + Item20"
 
 initialModels <-
   c(#realModel,
-    startModel2,
+    # startModel2,
     startModel3,
     # startModel4,
     startModel4a,
-    # startModel4b,
-    startModel5a,
+    # startModel4c,
+    # startModel5a,
     startModel5b)
 
 lavaanDesc <-
@@ -88,17 +94,20 @@ lavaanDesc <-
 testMax <-
   exploratorySA(initialModels = initialModels,
                 originalData = initialData,
-                maxSteps = 1000,
+                maxSteps = 3000,
                 fitStatistic = 'cfi',
                 lavaan.model.specs = lavaanDesc)
 
 testMin <-
   exploratorySA(initialModels = initialModels,
                 originalData = initialData,
-                maxSteps = 1000,
+                maxSteps = 3000,
                 fitStatistic = 'chisq',
                 maximize = F,
                 lavaan.model.specs = lavaanDesc)
 testMax
 testMin
 print(modelTableToString(bestECFA(testMax)))
+print(modelTableToString(bestECFA(testMin)))
+summary(testMax)
+summary(testMin)

7c2b51f302e838dcb54a99c91c70801a81be4145
Anthony Raborn <anthony.w.raborn@gmail.com>
2021-05-17 22:11:29 -0400

    Added allArgs function found on stackoverflow to capture the explicit values used during exploratorySA() evaluation

--- R/internals.R
+++ R/internals.R
@@ -688,6 +688,36 @@ fitStatTestCheck <-
     checkIfEval
   }
 
+# allArgs ####
+allArgs <- function(orig_values = FALSE) {
+  # source: https://stackoverflow.com/a/47955845
+  # get formals for parent function
+  parent_formals <- formals(sys.function(sys.parent(n = 1)))
+
+  # Get names of implied arguments
+  fnames <- names(parent_formals)
+
+  # Remove '...' from list of parameter names if it exists
+  fnames <- fnames[-which(fnames == '...')]
+
+  # Get currently set values for named variables in the parent frame
+  args <- evalq(as.list(environment()), envir = parent.frame())
+
+  # Get the list of variables defined in '...'
+  args <- c(args[fnames], evalq(list(...), envir = parent.frame()))
+
+
+  if(orig_values) {
+    # get default values
+    defargs <- as.list(parent_formals)
+    defargs <- defargs[unlist(lapply(defargs, FUN = function(x) class(x) != "name"))]
+    args[names(defargs)] <- defargs
+    setargs <- evalq(as.list(match.call())[-1], envir = parent.frame())
+    args[names(setargs)] <- setargs
+  }
+  return(args)
+}
+
 utils::globalVariables(
   c(
     'auto.cov.lv.x',
